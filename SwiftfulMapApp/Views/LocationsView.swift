7b01febd626dc5a50d8896949d41074ed2bce2ab
jgestradam <jgestradam@unal.edu.co>
2022-11-28 20:39:53 -0500

    SwiftUI Map App #8

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -10,8 +10,8 @@ import MapKit
 
 struct LocationsView: View {
     
-    //@StateObject private var vm = LocationsViewModel()
     @EnvironmentObject private var vm: LocationsViewModel
+    let maxWidthForIpad: CGFloat = 700
     
     var body: some View {
         ZStack {
@@ -21,6 +21,7 @@ struct LocationsView: View {
             VStack(spacing: 0) {
                 header
                     .padding()
+                    .frame(maxWidth: maxWidthForIpad)
                 Spacer()
                 locationsPreviewStack
             }
@@ -91,6 +92,8 @@ extension LocationsView {
                     LocationPreviewView(location: location)
                         .shadow(color: Color.black.opacity(0.3), radius: 20)
                         .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
                         .transition(.asymmetric(
                             insertion: .move(edge: .trailing),
                             removal: .move(edge: .leading)))
